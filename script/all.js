2266b7e62f8287c1f7f2bea2cb96d49ae67d7816
BA4IHR <208216462+MSK-10032@users.noreply.github.com>
2026-03-28 22:36:44 +0800

    script

--- script/all.js
+++ script/all.js
@@ -1,4 +1,4 @@
-function getURL(name) {
+﻿function getURL(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
         results = regex.exec(location.search);
